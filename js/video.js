0efc6c15df7427537d20d4fadf9d8b62700a3084
Ronan Prugh <rprugh@umich.edu>
2022-11-01 13:39:48 -0400

    for iniitial turn in

--- js/video.js
+++ js/video.js
@@ -4,6 +4,7 @@ function addListeners() {
 	document.querySelector("#play").addEventListener("click", function() {
 		console.log("Play Video");
 		video.play();
+		document.getElementById("volume").innerHTML = (video.volume * 100).toString() + '%';
 	});
 
 	document.querySelector("#pause").addEventListener("click", function() {
@@ -21,14 +22,38 @@ function addListeners() {
 		video.playbackRate /= 0.9;
 		if (Math.abs(video.playbackRate - 1) < 0.000001) video.playbackRate = 1;
 		console.log("Speed is " + video.playbackRate);
-	})
-	
+	});
+
+	document.querySelector('#skip').addEventListener("click", function() {
+		video.currentTime = (video.currentTime + 10 > video.duration) ? 0 : video.currentTime + 10;
+		console.log("Current Time is " + video.currentTime);
+	});
+
+	document.querySelector('#mute').addEventListener("click", function(event) {
+		video.mute = (video.mute) ? false : true;
+		event.target.innerHTML = event.target.innerHTML == "Mute" ? "Unmute" : "Mute";
+		console.log("Video Mute is: " + video.mute);
+	});
+
+	document.querySelector('#slider').addEventListener("change", function(event) {
+		video.volume = event.target.value / 100;
+	});
+
+	document.querySelector('#vintage').addEventListener("click", function() {
+		video.classList.add('oldSchool');
+	});
+
+	document.querySelector('#orig').addEventListener("click", function() {
+		video.classList.remove('oldSchool');
+	});
 
 }
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
 	video = document.querySelector('video');
+	video.loop = false;
+	video.autoplay = false;
 	video.load();
 	addListeners();
 });
